45e9e3ce9e8e6a28e3f5e05d7d8bd1c0cf8ebbe1
Varun Dodla <varun@dodla.com>
2013-10-21 04:34:41 +0000

    fixed polyfill for String.toTitleCase

--- app/polyfills.js
+++ app/polyfills.js
@@ -1,7 +1,14 @@
 "use strict";
 /* String additions */
-String.prototype.toTitleCase = function() {
-  return this.replace(/\w\S*/g, function(match) {
-    return match.charAt(0).toUpperCase() + match.substr(1).toLowerCase();
-  });
-};
+
+if (!String.prototype.toTitleCase) {
+  String.prototype.toTitleCase = function() {
+    if (!this || typeof this !== 'string') {
+      return '';
+    }
+
+    return this.replace(/\w\S*/g, function(match) {
+      return match.charAt(0).toUpperCase() + match.substr(1).toLowerCase();
+    });
+  };
+}
